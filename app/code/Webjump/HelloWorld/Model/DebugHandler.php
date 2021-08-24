58962f5328b7a73ac5288b4adb2377f76aee9f7a
Gustavo Santos Melo <gsantos15569@gmail.com>
2021-08-23 23:31:53 -0300

    configuration about a custom debug

--- app/code/Webjump/HelloWorld/Model/DebugHandler.php
+++ app/code/Webjump/HelloWorld/Model/DebugHandler.php
@@ -5,6 +5,6 @@ use Magento\Framework\Logger\Handler\Base;
 use Monolog\Logger;
 
 class DebugHandler extends Base {
-    protected $fileName = '/var/log/debug_custom.log' || '/var/log/critical_custom.log';
+    protected $fileName = '/var/log/debug_custom.log';
     protected $loggerType = Logger::DEBUG;
 }
