f58396ce8e4ce665706c1b24f62cf565f83d68eb
Gustavo Santos Melo <gsantos15569@gmail.com>
2021-08-27 22:53:40 -0300

    add index raw view

---
+++ app/code/Webjump/HelloWorld/Controller/Index/Index.php
@@ -0,0 +1,33 @@
+<?php
+namespace Webjump\HelloWorld\Controller\Index;
+
+use Magento\Framework\Controller\ResultFactory;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+    /**
+     * @var \Magento\Framework\View\Result\PageFactory
+     */
+    protected $_pageFactory;
+
+    /**
+     * @param \Magento\Framework\App\Action\Context $context
+     */
+    public function __construct(
+       \Magento\Framework\App\Action\Context $context,
+       \Magento\Framework\View\Result\PageFactory $pageFactory
+    )
+    {
+        $this->_pageFactory = $pageFactory;
+        return parent::__construct($context);
+    }
+    /**
+     * View page action
+     *
+     * @return \Magento\Framework\Controller\ResultInterface
+     */
+    public function execute()
+    {
+        return $this->_pageFactory->create(ResultFactory::TYPE_RAW);
+    }
+}
