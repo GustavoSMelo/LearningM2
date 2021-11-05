e511be8240bfe96b283715c05271b7aa933cf0ef
Gustavo Santos Melo <gsantos15569@gmail.com>
2021-11-04 21:09:42 -0300

    Model of toy

---
+++ app/code/MyVendor/HelloWorld/Model/Toy.php
@@ -0,0 +1,50 @@
+<?php
+namespace MyVendor\HelloWorld\Model;
+
+use MyVendor\HelloWorld\Model\ResourceModel\Toy as ResourceModelToy;
+
+class Toy extends \Magento\Framework\Model\AbstractModel
+{
+    const CACHE_TAG = 'myvendor_helloworld_toy';
+
+    /**
+     * Model cache tag for clear cache in after save and after delete
+     *
+     * @var string
+     */
+    protected $_cacheTag = self::CACHE_TAG;
+
+    /**
+     * Prefix of model events names
+     *
+     * @var string
+     */
+    protected $_eventPrefix = 'toy';
+
+    /**
+     * @param \Magento\Framework\Model\Context $context
+     * @param \Magento\Framework\Registry $registry
+     * @param \Magento\Framework\Model\ResourceModel\AbstractResource $resource
+     * @param \Magento\Framework\Data\Collection\AbstractDb $resourceCollection
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\Model\Context $context,
+        \Magento\Framework\Registry $registry,
+        \Magento\Framework\Model\ResourceModel\AbstractResource $resource = null,
+        \Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,
+        array $data = []
+    ) {
+        parent::__construct($context, $registry, $resource, $resourceCollection, $data);
+    }
+
+    /**
+     * Initialize resource model
+     *
+     * @return void
+     */
+    protected function _construct()
+    {
+        $this->_init(ResourceModelToy::class);
+    }
+}
