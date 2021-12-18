488da5184dcc6405631315f61f9457e748724ba6
Gustavo Santos Melo <gsantos15569@gmail.com>
2021-12-18 17:20:16 -0300

    renamed fie

---
+++ app/code/MyVendor/HelloWorld/Setup/Data/CreateCustomEavAttribute.php
@@ -0,0 +1,61 @@
+<?php
+namespace MyVendor\HelloWorld\Setup\Data;
+
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+
+class CreateCustomEavAttribute implements DataPatchInterface
+{
+    private $eavSetupFactory;
+
+    private $setup;
+
+    public function __construct(EavSetupFactory $eavSetupFactory, ModuleDataSetupInterface $setup)
+    {
+        $this->eavSetupFactory = $eavSetupFactory;
+        $this->setup = $setup;
+    }
+
+    public function apply(): void
+    {
+        $this->setup->getConnection()->startSetup();
+
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->setup]);
+        $eavSetup->addAttribute(
+            \Magento\Catalog\Model\Product::ENTITY,
+            'custom_attribute_m2',
+            [
+                'type' => 'text',
+                'label' => 'Custom Attribute',
+                'input' => 'multiselect',
+                'source' => 'MyVendor\HelloWorld\Model\Config\Product\EavCustomAttribute',
+                'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_GLOBAL,
+                'visible' => true,
+                'required' => false,
+                'backend' => 'Magento\Eav\Model\Entity\Attribute\Backend\ArrayBackend',
+                'visible_on_front' => true,
+                'used_in_product_listing' => true,
+                'unique' => false,
+            ]
+        );
+
+        $this->setup->getConnection()->endSetup();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies(): array
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases(): array
+    {
+        return [];
+    }
+}
